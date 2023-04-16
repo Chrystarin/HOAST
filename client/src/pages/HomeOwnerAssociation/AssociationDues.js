a468789e784b716b0f86f1b53ea4f01a11af85d2
Gian Carlo Dela Cruz <59326022+Deuz1007@users.noreply.github.com>
2023-04-16 17:31:12 +0800

    Update AssociationDues.js

--- client/src/pages/HomeOwnerAssociation/AssociationDues.js
+++ client/src/pages/HomeOwnerAssociation/AssociationDues.js
@@ -1,22 +1,75 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 import NavBar from '../../layouts/NavBar';
 import Button from '@mui/material/Button';
 import './Dashboard.scss';
 import DuesIcon from '../../images/icons/due-date.png';
 import SideBar from './SideBar';
 import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import FormControl from '@mui/material/FormControl';
 
 import axios from '../../utils/axios';
 
 import './AssociationDues.scss';
 
 function AssociationDues() {
-	
+	const { id } = useParams();
+
+	const [stepper, setStepper] = useState(1);
+	const [hoa, setHoa] = useState();
+	const [homes, setHomes] = useState();
+	const [dues, setDues] = useState()
+
+	// Collection of form data
 	const [form, setForm] = useState({
+		hoaId: '',
 		homeId: '',
 		amount: '',
 		months: ''
 	});
+
+	useEffect(() => {
+		const fetchHoa = async () => {
+			await axios.get(`hoas`,{
+				params: {
+					hoaId: `${id}`
+				}
+			}).then((response) => {
+				setHoa(response.data);
+
+				const fetchHomes = async () => {
+					await axios.get(`homes`, {
+						params: {
+							homeId: response.data.homeId
+						}
+					}
+					).then((response) => {
+						setHomes(response.data);
+						console.log(response.data);
+					});
+				};
+				const fetchDues = async () => {
+					await axios.get(`dues`, {
+						params: {
+							homeId: response.data.homeId
+						}
+					}).then((response) => {
+						setDues(response.data);
+						console.log(response.data);
+					});
+				};
+
+				fetchHomes();
+				fetchDues();
+			});
+		};
+
+		fetchHoa();
+	}, []);
 
 	function updateForm(e) {
 		return setForm((prev) => {
@@ -28,13 +81,12 @@ function AssociationDues() {
 
 	async function Submit(e) {
 		e.preventDefault();
-
 		try {
-			// Login
 			await axios
 				.post(
 					`dues`,
 					JSON.stringify({
+						hoaId: `${id}`,
 						homeId: form.homeId,
 						amount: form.amount,
 						months: form.months
@@ -51,39 +103,83 @@ function AssociationDues() {
 		}
 	}
 
-	return (
-		<div>
-			<form onSubmit={Submit}>
-                <TextField
-					id="filled-password-input"
-					label="amount"
-					type="text"
-					autoComplete="current-password"
-					variant="filled"
-					onChange={(e) => updateForm({ amount: e.target.value })}
-				/>
-				<TextField
-					id="filled-password-input"
-					label="dueDate"
-					type="date"
-					autoComplete="current-password"
-					variant="filled"
-					onChange={(e) => updateForm({ month: e.target.value })}
-				/>
-				<div className="Form__Button">
-					<Button variant="text">Cancel</Button>
-					<Button
-						variant="contained"
-						type="submit"
-						className="Submit"
-					>
-						Submit
-					</Button>
-				</div>
-			</form>
-		</div>
-	);
+	function Stepper(props){
+		switch (stepper){
+			case 1:
+				return (
+					<div>
+						<form onSubmit={Submit} className="Form">
+							{/* Shows HomeIDs */}
+							<FormControl fullWidth>
+								<InputLabel
+									variant="filled"
+									id="home-select"
+								>
+									Home
+								</InputLabel>
+								<Select
+									labelId="home-select"
+									value={form.homeId}
+									label="Home"
+									onChange={(e) =>
+										setForm({
+											...form,
+											homeId: e.target.value
+										})
+									}
+								>
+									{props.homes.length > 0 &&
+										props.homes.map((home) => {
+											return (
+												<MenuItem
+													key={home.homeId}
+													value={home.homeId}
+												>
+													{home.name}
+												</MenuItem>
+											);
+										})}
+								</Select>
+							</FormControl>
+			
+							<TextField
+								id="filled-password-input"
+								label="amount"
+								type="text"
+								autoComplete="current-password"
+								variant="filled"
+								onChange={(e) => updateForm({ amount: e.target.value })}
+							/>
+							<TextField
+								id="filled-password-input"
+								label="paidUntil"
+								type="date"
+								autoComplete="current-password"
+								variant="filled"
+								onChange={(e) => updateForm({ month: e.target.value })}
+							/>
+							<div className="Form__Button">
+								<Button variant="text">Cancel</Button>
+								<Button
+									variant="contained"
+									type="submit"
+									className="Submit"
+								>
+									Submit
+								</Button>
+							</div>
+						</form>
+					</div>
+				);
+				break;
+			default:
+				break;
+		}
+	}
 
+	// if (!dues) return <div>Loading...</div>;
+
+	
 	// return <>
 	//     <NavBar/>
 	//     <div id='SectionHolder'>
