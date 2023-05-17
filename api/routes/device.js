c592e26ef2e23146b221a88caacc5b2efc99e491
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2023-05-17 17:11:19 +0800

    Update device.js

--- api/routes/device.js
+++ api/routes/device.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
+const asyncHandler = require('../middlewares/asyncHandler');
+
 const { addDevice } = asyncHandler(require('../controllers/deviceController'));
 
 router.post('/', addDevice);
