c427bd4c141c8dc85c120716961f11b1dd45da83
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2023-05-17 19:59:16 +0800

    client deployment config

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4000',
+    // baseURL: 'http://localhost:4000',
+    baseURL: process.env.REACT_APP_API_URL
     timeout: 5000,
     withCredentials: true,
     headers: {
