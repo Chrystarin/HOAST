4186dbd82976793256d210829eb87298619e35fb
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2023-05-18 13:43:41 +0800

    device config

--- api/controllers/deviceController.js
+++ api/controllers/deviceController.js
@@ -6,26 +6,30 @@ const bcrypt = require('bcrypt');
 const addDevice = async (req, res, next) => {
 	const { email, password, hoaId, deviceIP } = req.body;
 
+    console.log("ENTERED")
+
+    return res.json(deviceIP);
+
 	// Find user by email and check password using bcrypt
-	const user = await User.findOne({ email });
-	console.log(user.credentials.password)
-	if (!user || !bcrypt.compareSync(password, user.credentials.password))
-		throw new UnauthorizedError('Invalid user credentials');
+	// const user = await User.findOne({ email });
+	// console.log(user.credentials.password)
+	// if (!user || !bcrypt.compareSync(password, user.credentials.password))
+	// 	throw new UnauthorizedError('Invalid user credentials');
 
-	// Check if user is admin or guard of hoaId
-	const hoa = await HOA.findOne({
-		hoaId,
-		$or: [
-			{ admin: user._id },
-			{ 'guards.user': user._id, 'guards.status': 'active' }
-		]
-	});
-	if (!hoa) throw new ForbiddenError('User not guard or admin of HOA');
+	// // Check if user is admin or guard of hoaId
+	// const hoa = await HOA.findOne({
+	// 	hoaId,
+	// 	$or: [
+	// 		{ admin: user._id },
+	// 		{ 'guards.user': user._id, 'guards.status': 'active' }
+	// 	]
+	// });
+	// if (!hoa) throw new ForbiddenError('User not guard or admin of HOA');
 
-	hoa.deviceIP = deviceIP;
-	await hoa.save();
+	// hoa.deviceIP = deviceIP;
+	// await hoa.save();
 
-	res.status(201).send('Device added');
+	// res.status(201).send('Device added');
 };
 
 module.exports = { addDevice };
