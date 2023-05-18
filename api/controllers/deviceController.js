1f28a51fe0bf4e7aa87592a43621f00e43f9aae8
haroldcastillo <44398016+haroldcastillo@users.noreply.github.com>
2023-05-18 19:21:29 +0800

    device config

--- api/controllers/deviceController.js
+++ api/controllers/deviceController.js
@@ -1,4 +1,4 @@
-const { UnauthorizedError, ForbiddenError, NotFoundError } = require('../helpers/errors');
+const { UnauthorizedError, ForbiddenError } = require('../helpers/errors');
 const HOA = require('../models/HOA');
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
@@ -6,29 +6,24 @@ const bcrypt = require('bcrypt');
 const addDevice = async (req, res, next) => {
 	const { email, password, hoaId, deviceIP } = req.body;
 
-	console.log('Request Bodyn\n', req.body);
+	console.log(req.body);
 
 	// Find user by email and check password using bcrypt
-	const user = await User.findOne({ 'credentials.email': email });
+	const user = await User.findOne({ 'credentials.email': email }, { 'credentials.password': 1 });
 
-	console.log('User\n', req.user);
-
-	// Compare password
 	if (!user || !bcrypt.compareSync(password, user.credentials.password))
 		throw new UnauthorizedError('Invalid user credentials');
 
-	console.log('Compare succeeded');
+	// Check if user is admin or guard of hoaId
+	const hoa = await HOA.findOne({
+		hoaId,
+		$or: [
+			{ admin: user._id },
+			{ 'guards.user': user._id, 'guards.status': 'active' }
+		]
+	});
+	if (!hoa) throw new ForbiddenError('User not guard or admin of HOA');
 
-	// Find HOA
-	const hoa = await HOA.findOne({ hoaId }, { admin: 1, 'guards.user': 1 });
-	console.log('HOA\n', hoa)
-	if (!hoa) throw new NotFoundError('HOA not found');
-
-	// Check if user is admin or guard of hoa
-	if(!hoa.admin.equals(user._id) || !hoa.guards.find((guard) => guard.user.equals(user._id)))
-		throw new ForbiddenError('User not guard or admin of HOA');
-
-	// Save deviceIP to HOA
 	hoa.deviceIP = deviceIP;
 	await hoa.save();
 
