da1cf6c321b8bf85b92a9658804d065f0710e1dd
haroldcastillo <44398016+haroldcastillo@users.noreply.github.com>
2023-05-18 19:16:02 +0800

    device config

--- api/controllers/deviceController.js
+++ api/controllers/deviceController.js
@@ -6,15 +6,22 @@ const bcrypt = require('bcrypt');
 const addDevice = async (req, res, next) => {
 	const { email, password, hoaId, deviceIP } = req.body;
 
+	console.log('Request Bodyn\n', req.body);
+
 	// Find user by email and check password using bcrypt
 	const user = await User.findOne({ 'credentials.email': email });
+
+	console.log('User\n', req.user);
 
 	// Compare password
 	if (!user || !bcrypt.compareSync(password, user.credentials.password))
 		throw new UnauthorizedError('Invalid user credentials');
 
+	console.log('Compare succeeded');
+
 	// Find HOA
 	const hoa = await HOA.findOne({ hoaId }, { admin: 1, 'guards.user': 1 });
+	console.log('HOA\n', hoa)
 	if (!hoa) throw new NotFoundError('HOA not found');
 
 	// Check if user is admin or guard of hoa
