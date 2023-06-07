d7ddb77bd653f99879b18a83fd493fc037330c39
Gian Carlo Dela Cruz <59326022+Deuz1007@users.noreply.github.com>
2023-06-07 09:11:05 +0800

    Revert "device credentials check"
    This reverts commit 9e4cc0bcaa3434daa0840483d3092cfda9685275.

--- api/controllers/deviceController.js
+++ api/controllers/deviceController.js
@@ -9,14 +9,15 @@ const addDevice = async (req, res, next) => {
 	console.log(req.body);
 
 	// Find user by email and check password using bcrypt
-	const user = await User.findOne(
-		{ 'credentials.email': email },
-		{ 'credentials.password': 1 }
-	);
+	// const user = await User.findOne(
+	// 	{ 'credentials.email': email },
+	// 	{ 'credentials.password': 1 }
+	// );
 
-	// const user = await User.findOne({ email });
+	const user = await User.findOne({ email });
 
 	console.log(user.credentials.password);
+
 	console.log(user);
 
 	console.log(bcrypt.compareSync(password, user.credentials.password));
