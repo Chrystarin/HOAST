fdc4bc1062acd560e72ffc8b9859adaaa2cb56f4
Gian Carlo Dela Cruz <59326022+Deuz1007@users.noreply.github.com>
2023-06-20 12:24:39 +0800

    color home icon

--- client/src/pages/Homes/Homes.js
+++ client/src/pages/Homes/Homes.js
@@ -10,85 +10,97 @@ import Menu from '@mui/material/Menu';
 import NativeSelect from '@mui/material/NativeSelect';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import loading from '../../images/loading.gif';
-import {useAuth} from '../../utils/AuthContext.js';
+import { useAuth } from '../../utils/AuthContext.js';
 import Filter from '../../components/Filter/Filter.js';
 
 function Homes() {
-  const {user, isAdmin} = useAuth();
-  const [homes, setHomes] = useState();
-  const [data,setData] = useState({});
+	const { user, isAdmin } = useAuth();
+	const [homes, setHomes] = useState();
+	const [data, setData] = useState({});
 
-  // States for popup filter
-  const [filterValue,setFilterValue] = useState(
-    {
-      sortBy:"A_Z"
-    }
-  );
+	// States for popup filter
+	const [filterValue, setFilterValue] = useState({
+		sortBy: 'A_Z'
+	});
 
-
-  useEffect(() => {
+	useEffect(() => {
 		// Retrieves Homes
 		const fetchHomes = async () => {
-			await axios
-				.get(`homes`)
-				.then((response) => {
-					setHomes(response.data);
-				});
+			await axios.get(`homes`).then((response) => {
+				setHomes(response.data);
+			});
 		};
-    fetchHomes();
+		fetchHomes();
 	}, []);
-  
-  
 
+	// Returns if member is null
+	if (!homes)
+		return (
+			<>
+				<div className="Loading">
+					<img
+						src={loading}
+						alt=""
+					/>
+					<h3>Loading...</h3>
+				</div>
+			</>
+		);
 
-  // Returns if member is null
-	if (!homes) return <>
-    <div className='Loading'>
-      <img src={loading} alt="" />
-      <h3>Loading...</h3>
-    </div>
-  </>
-  
-  
+	return (
+		<>
+			<Navbar type="home" />
+			<div className="SectionHolder">
+				<section className="Section">
+					<h3 className="SectionTitleDashboard">Homes</h3>
+					<div className="SectionController">
+						<div id="SearchInput__Container">
+							<SearchInput
+								setData={setData}
+								data={homes}
+								keys={['name', 'hoa', 'owner.name']}
+								filterValue={filterValue}
+							/>
+						</div>
+						<Filter
+							value={filterValue}
+							setValue={setFilterValue}
+						/>
+						<Button
+							variant="contained"
+							href="/homes/add"
+						>
+							Add Home
+						</Button>
+					</div>
 
-  return <>
-    <Navbar type="home"/>
-    <div className='SectionHolder'>
-      <section className='Section'>
-        <h3 className='SectionTitleDashboard'>Homes</h3>
-        <div className='SectionController'>
-          <div id='SearchInput__Container'>
-            <SearchInput setData={setData} data={homes} keys={["name","hoa","owner.name"]}  filterValue={filterValue} />
-          </div>
-          <Filter value={filterValue} setValue={setFilterValue}/>
-          <Button variant="contained" href='/homes/add'>Add Home</Button>
-        </div>
-
-        <div className='SectionList'>
-          {(data.length === 0 )?
-              <p>No homes found!</p>
-            :
-            <>
-              {data.length > 0 &&
-                  data.map((home) => {
-                  return (
-                  <Card 
-                      type="Home"
-                      key={home.homeId}
-                      id={home.homeId}
-                      title={home.name}
-                      subTitle1={home.address.number}
-                      subTitle2={home.address.street}
-                      url={`/homes/${home.homeId}`}
-                  />
-                  );
-              })}
-            </>
-          }
-        </div>
-      </section>
-    </div>
-  </>
+					<div className="SectionList">
+						{data.length === 0 ? (
+							<p>No homes found!</p>
+						) : (
+							<>
+								{data.length > 0 &&
+									data.map((home) => {
+										return (
+											<Card
+												type="Home"
+												key={home.homeId}
+												id={home.homeId}
+												title={home.name}
+												color={home.color}
+												subTitle1={home.address.number}
+												subTitle2={home.address.street}
+												url={`/homes/${home.homeId}`}
+											/>
+										);
+									})}
+							</>
+						)}
+					</div>
+				</section>
+			</div>
+		</>
+	);
 }
 
 export default Homes;
