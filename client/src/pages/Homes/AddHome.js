95b77f10df6864958431f5f1a3da686b5edf2453
Gian Carlo Dela Cruz <59326022+Deuz1007@users.noreply.github.com>
2023-06-20 08:05:40 +0800

    Update AddHome.js

--- client/src/pages/Homes/AddHome.js
+++ client/src/pages/Homes/AddHome.js
@@ -96,7 +96,7 @@ function AddHome() {
                     street: form.street,
                     phase: form.phase,
                     color: form.color,
-                    contactNumber: form.contactNumber
+                    contactNo: form.contactNumber
                 })
             )
             .then((response) => {
